8d224a30d11e34904225cd9d18e5b0e85300178d
leszekg <gonczar@gmail.com>
2017-11-22 14:09:24 +0000

    Create an Azure Service Bus namespace with one topic and subscription

---
+++ main.tf
@@ -0,0 +1,34 @@
+locals {
+  namespace_name      = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-${var.env}"
+}
+
+resource "azurerm_resource_group" "servicebus_resource_group" {
+  name     = "${local.resource_group_name}"
+  location = "${var.location}"
+}
+
+module "namespace" {
+  // TODO: reference a specific release
+  source                = "git@github.com:hmcts/terraform-module-servicebus-namespace.git"
+  name                  = "${local.namespace_name}"
+  location              = "${var.location}"
+  resource_group_name   = "${local.resource_group_name}"
+}
+
+module "private_beta_activation_topic" {
+  // TODO: reference a specific release
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic.git"
+  name                  = "reform.private-beta.account.activation"
+  resource_group_name   = "${local.namespace_name}"
+  namespace_name        = "${local.resource_group_name}"
+}
+
+module "private_beta_activation_subscription" {
+  // TODO: reference a specific release
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription.git"
+  name                  = "reform.private-beta.account.activation"
+  resource_group_name   = "${local.namespace_name}"
+  namespace_name        = "${local.resource_group_name}"
+  topic_name            = "${module.private_beta_activation_topic.name}"
+}
